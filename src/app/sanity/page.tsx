f025a7d17f5999ea02736d2b7b16b4d688f5b22b
jade0615 <jade0615@users.noreply.github.com>
2026-02-07 15:15:32 +0800

    debug: add sanity check routes to troubleshoot 404

---
+++ src/app/sanity/page.tsx
@@ -0,0 +1,9 @@
+
+export default function SanityPage() {
+    return (
+        <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+            <h1>Sanity Check Page</h1>
+            <p>If you see this, basic routing is working.</p>
+        </div>
+    );
+}
